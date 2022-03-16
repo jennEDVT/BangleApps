50759272ea99cf260958e82e6df1c254a97d343c
Jennifer Baudreau <jbaudreau@gmail.com>
2022-03-16 10:23:54 -0400

    Update app.js

--- apps/simple_clock/app.js
+++ apps/simple_clock/app.js
@@ -1,5 +1,6 @@
 var Layout = require("Layout");
 var count = 0;
+var count2 = 0;
 
 var layout = new Layout( {
   type:"v", c: [
@@ -8,12 +9,16 @@ var layout = new Layout( {
     {type:"txt", font:"20%", label:"12:00", id:"time" },
     {type:"txt", font:"9%", label:"The Date", id:"date" },
     {type:"txt", font:"10%", label:"", id:"padding2" },
-    {type:"txt", font:"8%", label:count , id:"btnResponse" },
-    
-        {type:"txt", font:"6x8:2", label:"", id:"padding3" },
-
-    
-     {type:"btn", font:"6x8:2", label:"Settings", cb: l=>openSettings() },
+    {type:"h", c: [
+       {type:"txt", font:"8%", label:count , id:"btnResponse" },
+       {type:"txt", font:"10%", label:"    ", id:"padding5" },
+       {type:"txt", font:"8%", label:count , id:"btn2Response" },
+    ]},    
+    {type:"txt", font:"3%", label:"", id:"padding3" },
+    {type:"h", c: [
+      {type:"btn", pad:8, font:"6x8:2", label:"Set", cb: l=>openSettings() },
+      {type:"btn",  pad:8, font:"6x8:2", fillx: 1, label:"Count", cb: l=>setLabel2() },
+    ]}    
   ]
 }, {btns:[
   {label:"", cb: l=>setLabel(),  cbl: l=>Bangle.showLauncher()},  
@@ -42,6 +47,13 @@ function setLabel() {
   Bangle.buzz(100,0.2);
   count = count + 1;
   layout.btnResponse.label = count;
+  layout.render();
+}
+
+function setLabel2() {
+  Bangle.buzz(100,0.2);
+  count2 = count2 + 1;
+  layout.btn2Response.label = count2;
   layout.render();
 }
 
